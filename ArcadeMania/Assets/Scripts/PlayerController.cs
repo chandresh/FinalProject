6c89f38f6167b649d0f0dc4372ef562f9b2ba934
Chandresh Pant <chandresh.pant@gmail.com>
2023-06-19 18:16:50 +0530

    Fix extra jump

--- ArcadeMania/Assets/Scripts/PlayerController.cs
+++ ArcadeMania/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     // Rigidbody of the player
     private Rigidbody2D playerRb;
+    private bool grounded = true;
 
     [SerializeField]
     float playerSpeed, playerJump;
@@ -27,9 +28,10 @@ public class PlayerController : MonoBehaviour
 
     private void JumpPlayer()
     {
-        if (Input.GetAxis("Jump") > 0)
+        if (Input.GetAxis("Jump") > 0 && grounded)
         {
             playerRb.velocity = new Vector2(playerRb.velocity.x, playerJump);
+            grounded = false;
         }
     }
 
@@ -42,15 +44,24 @@ public class PlayerController : MonoBehaviour
 
     private void FlipPlayer(float playerMovement)
     {
-
+        // If Player is moving right
         if (playerMovement > 0.01f)
         {
             transform.localScale = Vector3.one;
         }
 
+        // If Player is moving left
         if (playerMovement < -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            grounded = true;
         }
     }
 }
