c75b93d7a9687e4012463d3758d63752c8337e59
Chandresh Pant <chandresh.pant@gmail.com>
2023-06-19 17:49:11 +0530

    Move player horizontally

---
+++ ArcadeMania/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Rigidbody of the player
+    private Rigidbody2D playerRb;
+
+    [SerializeField]
+    float playerSpeed = 10;
+
+    private void Awake()
+    {
+        // Set the playerRb with the Rigidbody Component
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Capture the player movement (1 or -1)
+        float playerMovement = Input.GetAxis("Horizontal");
+
+        // Set player velocity
+        float playerVelocity = playerMovement * playerSpeed;
+        playerRb.velocity = new Vector2(playerSpeed, 0);
+    }
+
+}
