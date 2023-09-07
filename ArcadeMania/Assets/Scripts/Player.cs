9cc1eeee12949bef9d9bcca6b4325e88a99f05d9
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-07 08:58:01 +0530

    Consolidate all code

---
+++ ArcadeMania/Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int coins = 0;
+    public float speed = 5.0f;
+
+    public Text keyAmount;
+    public Text youWin;
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+
+        if (coins == 3)
+        {
+            Destroy(door);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            coins++;
+            keyAmount.text = "Coins: " + coins;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "MainKey")
+        {
+            youWin.text = "You got the key!!!";
+        }
+        if (collision.gameObject.tag == "Fire")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
