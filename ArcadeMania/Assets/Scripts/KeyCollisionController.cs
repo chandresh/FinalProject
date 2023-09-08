092fe9e0d8af00f1791262c33177d4380d9ab256
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-08 20:40:48 +0530

    Update Last level

--- ArcadeMania/Assets/Scripts/KeyCollisionController.cs
+++ ArcadeMania/Assets/Scripts/KeyCollisionController.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class KeyCollisionController : MonoBehaviour
 {
+    GameData gameData;
+    void Start()
+    {
+        gameData = new GameData();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         // If the other object has a tag Player
         if (other.CompareTag("Player"))
         {
+            // Save Game Data
+            gameData.UpdateHighestRound(3);
             // Next scene
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
