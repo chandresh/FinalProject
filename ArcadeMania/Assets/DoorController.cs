a3cc90e3691c4a944565a75eb277c8eb3941be5d
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-08 17:14:21 +0530

    Round Logic fixed

--- ArcadeMania/Assets/DoorController.cs
+++ ArcadeMania/Assets/DoorController.cs
@@ -4,12 +4,24 @@ using UnityEngine.SceneManagement;
 public class DoorController : MonoBehaviour
 {
 
+    GameData gameData;
+
+    void Start()
+    {
+        gameData = new GameData();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the player has entered the door using compareTag
         if (other.CompareTag("Player") || other.CompareTag("PlayerParts"))
         {
             Debug.Log("DoorController OnTriggerEnter2D Player");
+
+            // Save Game Data
+            gameData.UpdateHighestRound(2);
+            // TODO: Update highest score
+            gameData.SaveData();
 
             // Load the next scene
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
