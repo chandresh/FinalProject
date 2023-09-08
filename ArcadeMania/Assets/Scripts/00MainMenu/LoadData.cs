7f770e45d1f9392e0ccb17c00d5743c72b194d17
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-08 16:20:17 +0530

    Intro Screen Design

--- ArcadeMania/Assets/Scripts/00MainMenu/LoadData.cs
+++ ArcadeMania/Assets/Scripts/00MainMenu/LoadData.cs
@@ -13,20 +13,6 @@ public class LoadData : MonoBehaviour
         Debug.Log("Highest Round: " + gameData.HighestRound);
         int currentLevel = gameData.HighestRound;
 
-        // Deactivate highlight for all TextManager objects
-        foreach (GameObject text in levelTexts)
-        {
-            TextManager tm = text.GetComponent<TextManager>();
-            if (tm != null)
-            {
-                tm.isHighlighted = false;
-            }
-        }
-
-        // Activate highlight for the highest round
-        TextManager currentTM = levelTexts[currentLevel].GetComponent<TextManager>();
-        currentTM.isHighlighted = true;
-
     }
 
     // Update is called once per frame
