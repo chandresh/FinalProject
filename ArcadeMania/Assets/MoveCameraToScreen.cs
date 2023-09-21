85e8e0562e1ce97b649be60d8f0c161f8e1d71a8
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-21 19:42:59 +0530

    Fixed issue of getting lots of ghosts

--- ArcadeMania/Assets/MoveCameraToScreen.cs
+++ ArcadeMania/Assets/MoveCameraToScreen.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class MoveCameraToScreen : MonoBehaviour
 {
@@ -7,11 +9,24 @@ public class MoveCameraToScreen : MonoBehaviour
     public Transform player;
 
     private int currentZone = 0;  // The current camera zone index
+
+    // Store a list of enemies for each zone
+    // This is to fix the issue of enemies keep increasing
+    // when the player moves back and forth between zones
+    private List<GameObject>[] zoneEnemies;
     private Transform cameraTransform;
 
     void Start()
     {
         cameraTransform = GetComponent<Transform>();
+
+        // Initialize the list of enemies for each zone
+        zoneEnemies = new List<GameObject>[cameraZones.Length];
+
+        for (int i = 0; i < cameraZones.Length; i++)
+        {
+            zoneEnemies[i] = new List<GameObject>();
+        }
         SpawnEnemiesForZone(currentZone);
     }
 
@@ -22,12 +37,17 @@ public class MoveCameraToScreen : MonoBehaviour
         // Check if the player is out of the screen on the right side
         if (screenPos.x > 1 && currentZone < cameraZones.Length - 1)
         {
+            // Deactivate all enemies in the current zone
+            DeactivateEnemiesForZone(currentZone);
+
             currentZone++;
             SpawnEnemiesForZone(currentZone);
         }
         // Check if the player is out of the screen on the left side
         else if (screenPos.x < 0 && currentZone > 0)
         {
+            // Deactivate all enemies in the current zone
+            DeactivateEnemiesForZone(currentZone);
             currentZone--;
             SpawnEnemiesForZone(currentZone);
         }
@@ -48,19 +68,49 @@ public class MoveCameraToScreen : MonoBehaviour
 
     void SpawnEnemiesForZone(int zone)
     {
-        int enemyCount = UnityEngine.Random.Range(2, 8);
-
-        for (int i = 0; i < enemyCount; i++)
+        // Only spawn enemies if the list for this zone is empty
+        if (zoneEnemies[zone].Count == 0)
         {
-            GameObject enemy = EnemyPool.Instance.GetEnemy();
-            Vector3 spawnPos = new Vector3(
-                UnityEngine.Random.Range(cameraZones[zone].position.x - 5, cameraZones[zone].position.x + 10),
-                UnityEngine.Random.Range(cameraZones[zone].position.y - 6, cameraZones[zone].position.y - 8),
-                0
-            );
+            int enemyCount = UnityEngine.Random.Range(2, 8);
 
-            enemy.transform.position = spawnPos;
-            enemy.SetActive(true);
+            for (int i = 0; i < enemyCount; i++)
+            {
+                GameObject enemy = EnemyPool.Instance.GetEnemy();
+                Vector3 spawnPos = new Vector3(
+                    UnityEngine.Random.Range(cameraZones[zone].position.x - 5, cameraZones[zone].position.x + 10),
+                    UnityEngine.Random.Range(cameraZones[zone].position.y - 6, cameraZones[zone].position.y - 8),
+                    0
+                );
+
+                enemy.transform.position = spawnPos;
+                enemy.SetActive(true);
+                zoneEnemies[zone].Add(enemy); // Add the enemy to the list for this zone
+            }
+        }
+        else
+        {
+            // Reactivate enemies for this zone
+            foreach (GameObject enemy in zoneEnemies[zone])
+            {
+                enemy.SetActive(true);
+            }
         }
     }
+
+    void DeactivateEnemiesForZone(int zone)
+    {
+        for (int i = zoneEnemies[zone].Count - 1; i >= 0; i--)
+        {
+            GameObject enemy = zoneEnemies[zone][i];
+
+            if (enemy == null)
+            {
+                zoneEnemies[zone].RemoveAt(i);
+                continue;
+            }
+
+            enemy.SetActive(false);
+        }
+    }
+
 }
